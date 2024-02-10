814b131409503bb5184ba590aca367bb8b527813
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2024-02-09 18:39:19 -0600

    RELEASE YEAH

--- src/main/java/com/jetpacker06/codysmod/register/AllBlocks.java
+++ src/main/java/com/jetpacker06/codysmod/register/AllBlocks.java
@@ -43,6 +43,7 @@ public class AllBlocks {
         return registerBlock(doorType + "_door", () -> new CustomDoorBlock(properties, requiresRedstone, doorMaterial));
     }
 
+    // MY BLOCKS
     public static final RegistryObject<Block> BLACK_NETHER_BRICKS = registerBlock("black_nether_bricks", () ->
             new Block(BlockBehaviour.Properties.copy(Blocks.NETHER_BRICKS)));
     public static final RegistryObject<Block> CHISELED_BLACK_NETHER_BRICKS = registerBlock("chiseled_black_nether_bricks", () ->
@@ -60,21 +61,6 @@ public class AllBlocks {
     public static final RegistryObject<Block> NETHER_BRICK_FENCE_GATE = registerBlock("nether_brick_fence_gate", () ->
             new FenceGateBlock(BlockBehaviour.Properties.copy(Blocks.NETHER_BRICK_FENCE)));
 
-
-    public static final RegistryObject<Block> CACTUS_BALE = registerBlock("cactus_bale", () ->
-            new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
-    public static final RegistryObject<Block> COCOA_BASKET = registerBlock("cocoa_basket", () ->
-            new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
-
-
-    public static final RegistryObject<Block> CRACKED_END_STONE_BRICKS = registerBlock("cracked_end_stone_bricks", () ->
-            new Block(BlockBehaviour.Properties.copy(Blocks.END_STONE_BRICKS)));
-    public static final RegistryObject<Block> CRACKED_MUD_BRICKS = registerBlock("cracked_mud_bricks", () ->
-            new Block(BlockBehaviour.Properties.copy(Blocks.MUD_BRICKS)));
-    public static final RegistryObject<Block> CRACKED_PURPUR = registerBlock("cracked_purpur", () ->
-            new Block(BlockBehaviour.Properties.copy(Blocks.PURPUR_BLOCK)));
-
-
     public static final RegistryObject<Block> ICE_BRICKS = registerBlock("ice_bricks", () ->
             new Block(BlockBehaviour.Properties.copy(Blocks.PACKED_ICE)));
     public static final RegistryObject<Block> CRACKED_ICE_BRICKS = registerBlock("cracked_ice_bricks", () ->
@@ -86,28 +72,44 @@ public class AllBlocks {
     public static final RegistryObject<Block> ICE_BRICK_SLAB = registerBlock("ice_brick_slab", () ->
             new SlabBlock(BlockBehaviour.Properties.copy(Blocks.PACKED_ICE)));
 
-
-    public static final RegistryObject<Block> GOLD_LANTERN = registerBlock("gold_lantern", () ->
-            new LanternBlock(BlockBehaviour.Properties.copy(Blocks.LANTERN)));
-    public static final RegistryObject<Block> GOLD_SOUL_LANTERN = registerBlock("gold_soul_lantern", () ->
-            new LanternBlock(BlockBehaviour.Properties.copy(Blocks.SOUL_LANTERN)));
-    public static final RegistryObject<Block> COPPER_LANTERN = registerBlock("copper_lantern", () ->
-            new LanternBlock(BlockBehaviour.Properties.copy(Blocks.LANTERN)));
-    public static final RegistryObject<Block> COPPER_SOUL_LANTERN = registerBlock("copper_soul_lantern", () ->
-            new LanternBlock(BlockBehaviour.Properties.copy(Blocks.SOUL_LANTERN)));
-
-
-    public static final RegistryObject<Block> GOLD_DOOR = registerDoorBlock("gold", false,
-            CustomDoorBlock.DoorMaterial.METAL, BlockBehaviour.Properties.copy(Blocks.IRON_DOOR).requiresCorrectToolForDrops());
-    public static final RegistryObject<Block> COPPER_DOOR = registerDoorBlock("copper", false,
-            CustomDoorBlock.DoorMaterial.METAL, BlockBehaviour.Properties.copy(Blocks.IRON_DOOR).requiresCorrectToolForDrops());
-    public static final RegistryObject<Block> DEEPSLATE_DOOR = registerDoorBlock("deepslate", false,
-            CustomDoorBlock.DoorMaterial.STONE, BlockBehaviour.Properties.copy(Blocks.IRON_DOOR).requiresCorrectToolForDrops());
+    // VARIANTS OF VANILLA
+    public static final RegistryObject<Block> CRACKED_END_STONE_BRICKS = registerBlock("cracked_end_stone_bricks", () ->
+            new Block(BlockBehaviour.Properties.copy(Blocks.END_STONE_BRICKS)));
+    public static final RegistryObject<Block> CRACKED_MUD_BRICKS = registerBlock("cracked_mud_bricks", () ->
+            new Block(BlockBehaviour.Properties.copy(Blocks.MUD_BRICKS)));
+    public static final RegistryObject<Block> CRACKED_PURPUR = registerBlock("cracked_purpur", () ->
+            new Block(BlockBehaviour.Properties.copy(Blocks.PURPUR_BLOCK)));
 
     public static final RegistryObject<Block> OBSIDIAN_STAIRS = registerBlock("obsidian_stairs", () ->
             new StairBlock(Blocks.OBSIDIAN::defaultBlockState, BlockBehaviour.Properties.copy(Blocks.OBSIDIAN)));
     public static final RegistryObject<Block> OBSIDIAN_SLAB = registerBlock("obsidian_slab", () ->
             new SlabBlock(BlockBehaviour.Properties.copy(Blocks.OBSIDIAN)));
+
+    public static final RegistryObject<Block> QUARTZ_WALL = registerBlock("quartz_wall", () ->
+            new WallBlock(BlockBehaviour.Properties.copy(Blocks.QUARTZ_BLOCK)));
+
+    // DECO
+//    public static final RegistryObject<Block> GOLD_LANTERN = registerBlock("gold_lantern", () ->
+//            new LanternBlock(BlockBehaviour.Properties.copy(Blocks.LANTERN)));
+//    public static final RegistryObject<Block> GOLD_SOUL_LANTERN = registerBlock("gold_soul_lantern", () ->
+//            new LanternBlock(BlockBehaviour.Properties.copy(Blocks.SOUL_LANTERN)));
+//    public static final RegistryObject<Block> COPPER_LANTERN = registerBlock("copper_lantern", () ->
+//            new LanternBlock(BlockBehaviour.Properties.copy(Blocks.LANTERN)));
+//    public static final RegistryObject<Block> COPPER_SOUL_LANTERN = registerBlock("copper_soul_lantern", () ->
+//            new LanternBlock(BlockBehaviour.Properties.copy(Blocks.SOUL_LANTERN)));
+//
+//    public static final RegistryObject<Block> GOLD_DOOR = registerDoorBlock("gold", false,
+//            CustomDoorBlock.DoorMaterial.METAL, BlockBehaviour.Properties.copy(Blocks.IRON_DOOR).requiresCorrectToolForDrops());
+//    public static final RegistryObject<Block> COPPER_DOOR = registerDoorBlock("copper", false,
+//            CustomDoorBlock.DoorMaterial.METAL, BlockBehaviour.Properties.copy(Blocks.IRON_DOOR).requiresCorrectToolForDrops());
+//    public static final RegistryObject<Block> DEEPSLATE_DOOR = registerDoorBlock("deepslate", false,
+//            CustomDoorBlock.DoorMaterial.STONE, BlockBehaviour.Properties.copy(Blocks.IRON_DOOR).requiresCorrectToolForDrops());
+
+    // OTHER
+    public static final RegistryObject<Block> CACTUS_BALE = registerBlock("cactus_bale", () ->
+            new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
+    public static final RegistryObject<Block> COCOA_BASKET = registerBlock("cocoa_basket", () ->
+            new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
 
     public static final RegistryObject<Block> INVERTED_POWERED_RAIL = registerBlock("inverted_powered_rail", () ->
             new InvertedPoweredRailBlock(BlockBehaviour.Properties.copy(Blocks.POWERED_RAIL).noOcclusion()), false);
